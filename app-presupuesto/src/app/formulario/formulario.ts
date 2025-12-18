6e52e61f299710335764b7d646d62113ac8f37ac
diazjohan98 <vasquez.johan@correounivalle.edu.co>
2025-12-18 16:43:59 -0500

    Enhanced form component by applying ngClass to dynamically change input and button colors depending on whether the entry is an income or an expense.

--- app-presupuesto/src/app/formulario/formulario.ts
+++ app-presupuesto/src/app/formulario/formulario.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-formulario',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './formulario.html',
   styleUrl: './formulario.css',
 })
 export class Formulario {
 
+  tipo:string="ingresoOperacion"
+
+  tipoOperacion(evento: Event){
+    const elementoSelect = evento.target as HTMLSelectElement;
+    this.tipo = elementoSelect.value;
+  }
 }
