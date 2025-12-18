016b48b576591d7d3376de5fd03a294ee09ab48e
diazjohan98 <vasquez.johan@correounivalle.edu.co>
2025-12-18 17:25:00 -0500

    Project completed: fully functional budget management application built with Angular.

--- app-presupuesto/src/app/formulario/formulario.ts
+++ app-presupuesto/src/app/formulario/formulario.ts
@@ -1,18 +1,47 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IngresoService } from '../ingreso/ingreso.service';
+import { EgresoService } from '../egreso/egreso.service';
+import { IngresoModel } from '../ingreso/ingreso.model';
+import { EgresosModel } from '../egreso/egreso.model';
 
 @Component({
   selector: 'app-formulario',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './formulario.html',
   styleUrl: './formulario.css',
 })
 export class Formulario {
+  tipo: string = 'ingresoOperacion';
 
-  tipo:string="ingresoOperacion"
+  descripcionInput: string | null = null;
+  valorInput: number | null = null;
 
-  tipoOperacion(evento: Event){
+  constructor(private ingresoServicio: IngresoService, private egresoServicio: EgresoService) {}
+
+  tipoOperacion(evento: Event) {
     const elementoSelect = evento.target as HTMLSelectElement;
     this.tipo = elementoSelect.value;
+  }
+
+  agregarValor() {
+    if (this.descripcionInput != null && this.valorInput != null) {
+      if (this.tipo === 'ingresoOperacion') {
+        this.ingresoServicio.ingresos.push(
+          new IngresoModel(this.descripcionInput, this.valorInput)
+        );
+      }
+      else{
+        this.egresoServicio.egresos.push(
+          new EgresosModel(this.descripcionInput, this.valorInput)
+        )
+      }
+    }
+    else {
+      console.log ('Por favor ingrese una descripción y un valor válido.');;
+    }
+    this.descripcionInput = null;
+    this.valorInput = null;
   }
 }
